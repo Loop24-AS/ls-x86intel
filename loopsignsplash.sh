3a8308dc518003f1e42792318d744433952e1581
eirikpa <156180377+eirikpa@users.noreply.github.com>
2026-05-15 14:57:39 +0200

    Refactor loopsignsplash.sh for theme setup and image scaling
    Updated the loopsignsplash.sh script to change the splash image path and simplify theme creation. Adjusted image scaling logic and removed redundant code for better clarity.

--- loopsignsplash.sh
+++ loopsignsplash.sh
@@ -1,85 +1,68 @@
 #!/bin/bash
 
-# Define variables
+set -e
+
+REPO_DIR="/home/loopsign/ls-x86intel"
 THEME_NAME="loopsign"
 THEME_DIR="/usr/share/plymouth/themes/$THEME_NAME"
-SCRIPT_FILE="$THEME_DIR/$THEME_NAME.script"
-PLYMOUTH_FILE="$THEME_DIR/$THEME_NAME.plymouth"
-SPLASH_IMAGE="/home/loopsign/ls-rpi5labwc/splash.png"
+SPLASH_IMAGE="$REPO_DIR/splash.png"
 
-# Create the theme directory
-sudo mkdir -p "$THEME_DIR"
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root:"
+    echo "sudo $0"
+    exit 1
+fi
 
-# Create the loopsign.plymouth file
-sudo bash -c "cat > $PLYMOUTH_FILE << EOL
+if [ ! -f "$SPLASH_IMAGE" ]; then
+    echo "ERROR: Missing splash image:"
+    echo "$SPLASH_IMAGE"
+    exit 1
+fi
+
+mkdir -p "$THEME_DIR"
+
+cp "$SPLASH_IMAGE" "$THEME_DIR/splash.png"
+
+cat > "$THEME_DIR/$THEME_NAME.plymouth" <<EOF
 [Plymouth Theme]
-Name=LoopSign theme
-Description=A custom Plymouth theme for LoopSign
+Name=LoopSign
+Description=LoopSign boot splash
 ModuleName=script
 
 [script]
 ImageDir=$THEME_DIR
-ScriptFile=$SCRIPT_FILE
-EOL"
+ScriptFile=$THEME_DIR/$THEME_NAME.script
+EOF
 
-# Create the loopsign.script file
-sudo bash -c "cat > $SCRIPT_FILE << 'EOL'
+cat > "$THEME_DIR/$THEME_NAME.script" <<'EOF'
+wallpaper_image = Image("splash.png");
 screen_width = Window.GetWidth();
 screen_height = Window.GetHeight();
 
-theme_image = Image(\"splash.png\");
-image_width = theme_image.GetWidth();
-image_height = theme_image.GetHeight();
+image_width = wallpaper_image.GetWidth();
+image_height = wallpaper_image.GetHeight();
 
-scale_x = image_width / screen_width;
-scale_y = image_height / screen_height;
+scale_x = screen_width / image_width;
+scale_y = screen_height / image_height;
 
-flag = 1;
-
-if (scale_x > 1 || scale_y > 1)
-{
-    if (scale_x > scale_y)
-    {
-        resized_image = theme_image.Scale (screen_width, image_height / scale_x);
-        image_x = 0;
-        image_y = (screen_height - ((image_height  * screen_width) / image_width)) / 2;
-    }
-    else
-    {
-        resized_image = theme_image.Scale (image_width / scale_y, screen_height);
-        image_x = (screen_width - ((image_width  * screen_height) / image_height)) / 2;
-        image_y = 0;
-    }
-}
-else
-{
-    resized_image = theme_image.Scale (image_width, image_height);
-    image_x = (screen_width - image_width) / 2;
-    image_y = (screen_height - image_height) / 2;
+if (scale_x > scale_y) {
+    scale = scale_x;
+} else {
+    scale = scale_y;
 }
 
-if (Plymouth.GetMode() != \"shutdown\")
-{
-    sprite = Sprite (resized_image);
-    sprite.SetPosition (image_x, image_y, -100);
-}
+resized_image = wallpaper_image.Scale(image_width * scale, image_height * scale);
 
-message_sprite = Sprite();
-message_sprite.SetPosition(screen_width * 0.1, screen_height * 0.9, 10000);
+resized_width = resized_image.GetWidth();
+resized_height = resized_image.GetHeight();
 
-fun message_callback (text) {
-    my_image = Image.Text(text, 1, 1, 1);
-    message_sprite.SetImage(my_image);
-    sprite.SetImage (resized_image);
-}
+wallpaper_sprite = Sprite(resized_image);
+wallpaper_sprite.SetX((screen_width - resized_width) / 2);
+wallpaper_sprite.SetY((screen_height - resized_height) / 2);
+wallpaper_sprite.SetZ(-100);
+EOF
 
-Plymouth.SetUpdateStatusFunction(message_callback);
-EOL"
+plymouth-set-default-theme "$THEME_NAME"
+update-initramfs -u
 
-# Copy the splash image to the theme directory
-sudo cp "$SPLASH_IMAGE" "$THEME_DIR/splash.png"
-
-# Set the new theme as the default and update the initramfs
-sudo plymouth-set-default-theme -R "$THEME_NAME"
-
-echo "Plymouth theme '$THEME_NAME' has been successfully installed and applied!"
+echo "LoopSign Plymouth theme installed and activated."
